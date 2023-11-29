b93e2b230d34d47d0ea7309d188234db986dc085
Chrisman Campos <chrisman.campos@gmail.com>
2023-11-29 20:10:44 -0300

    adicionando função e while

--- ProgramaMenu.py
+++ ProgramaMenu.py
@@ -16,6 +16,7 @@ def gerenciar_medicamentos():
     while horario_atual < meia_noite + timedelta(days=1):
         print(horario_atual.strftime("%H:%M"))
         horario_atual += timedelta(hours=tomar)
+
 def consulta_medica():
     num_carteirinha = int(input("Informe o número da sua carteirinha: "))
 
@@ -24,3 +25,31 @@ def consulta_medica():
 
 def contato():
     print("Integrantes:\nBruno Hermano - RM553799\nChrisman Campos - RM554026\nLeonardo Macedo - RM552753\n")
+
+def criar_conta():
+    # Guardando dados
+    nome = input("Informe seu nome completo: ")
+    cpf = int(input("Informe seu cpf: "))
+    rg = int(input("Informe seu RG: "))
+    data_nasc = input("Informe sua data de nascimento: ")
+    cidade = input("Informe sua cidade: ")
+    endereco = input("Informe seu endereço completo: ")
+
+    # Gerar o número de cadastro
+    print("O número da sua carteirinha é: ", random.randint(100, 99999))
+
+sair = 1
+while (sair != 0):
+    escolha = int(input("\nBem Vindo(a) a BCL Med!\n\nEscolha uma opção do menu:\n1-Gerenciamento de medicamentos\n2-Consulta médica\n3-Contato\n4-Criar conta\nEscolha:"))
+    match escolha:
+        case 1:
+            gerenciar_medicamentos()
+        case 2:
+            consulta_medica()
+        case 3:
+            contato()
+        case 4:
+            criar_conta()
+        case _:
+            print("Opção invalida")
+    sair = int(input("Deseja fazer outra interação? (0-Não 1-Sim): "))
